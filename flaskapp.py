584b9f7961d733186db66f3637b32fe0ed1efc8d
tanmaybhandge <111745693+tanmaybhandge@users.noreply.github.com>
2023-09-18 17:22:54 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,1 +1,13 @@
-print ("Hello World!  This is test application")
+from flask import Flask
+
+# Create a Flask app instance
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This Flask app is running on port 9877.'
+
+if __name__ == '__main__':
+    # Run the Flask app on port 9877
+    app.run(host='0.0.0.0', port=9877)
